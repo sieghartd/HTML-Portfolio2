7469f125f7b529c062511fc879de21fe05e51a4d
greg despeines <sieghartd@gmail.com>
2024-04-10 22:52:43 -0400

    Adding Rest API project

--- 5.5+REST+APIs/5.5 REST APIs/index.js
+++ 5.5+REST+APIs/5.5 REST APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "230f0099-b564-4f06-98ba-091e3686e9cb";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
@@ -35,23 +35,54 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  try{
+  const result = await axios.post(API_URL + "/secrets", req.body, config);
+  res.render("index.ejs", {content: JSON.stringify(result.data)});
+  } catch (error) {
+  res.render("index.ejs", {content: JSON.stringify(result.data)});
+  }
+  
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
+  try {
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  const result = await axios.put(API_URL + "/secrets/" + searchID + req.body + config);
+  res.render("index.ejs", {content: JSON.stringify(result.data)});
+} catch (error) {
+  res.render("index.ejs", {content: JSON.stringify(error.response.data)});
+}
 });
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  try {
+  const result = await axios.patch(
+    API_URL + "/secrets/" +searchId,
+    req.body,
+    config
+  );
+  res.render("index.ejs", {content: JSON.stringify(result.data)});
+} catch (error) {
+  res.render("index.ejs", {content: JSON.stringify(error.response.data)});
+}
 });
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+  try {
+  const result = await axios.delete(
+    API_URL + "/secrets/"+ searchId, config);
+  res.render("index.ejs", {content: JSON.stringify(result.data)});
+} catch (error) {
+  res.render("index.ejs", {content: JSON.stringify(error.response.data)});
+}
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
